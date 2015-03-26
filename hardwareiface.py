c93c4b759a5c7c1743cb2d1be5a19f7fad5a22bf
manickyoj <hotshot354@gmail.com>
2015-03-26 16:37:18 -0400

    harwareiface.py preliminarily complete, but needs testing, and not actually hooked to Arduino.

--- hardwareiface.py
+++ hardwareiface.py
@@ -1,15 +1,28 @@
-import serial
+"""
+hardwareiface.py
+----------------
 
+The interface which allows patterns to be played on the
+water curtain.
+
+IDEA: Clients for submitting patterns in democratized way.
+"""
+
+# ----- IMPORTS ----- #
+import serial
+import queue as q
 
 # ----- HARDWARE CONSTANTS ----- #
 COLS = 20  # Number of solenoids available
 ON_DURATION = 0.25  # Duration on per pattern row
 OFF_DURATION = 0.5  # Duration off between pattern rows
 
-
-# ----- HARDWARE CLASSES ----- #
+# ----- PATTERN CLASS ----- #
 class Pattern:
-    _hardware_locked = False
+    PLAY = {
+        "loop" : -1
+        "stop" : 0
+    }
 
     def __init__ (pattern, invert=true, alignment=None):
         """
@@ -19,8 +32,8 @@ class Pattern:
         """
 
         self.pattern = align(pattern)
-        self.pattern = pattern[::-1] # Reverse the vertical alignment of the pattern
-
+        self.pattern = self.pattern[::-1] # Reverse the vertical alignment of the pattern
+        self.play_counter = 0
 
 
     def align (self, pattern, alignment=None):
@@ -44,3 +57,59 @@ class Pattern:
                 pattern = pattern
             else:
                 return pattern
+
+    def play (self, times=1):
+        self.play_counter = Pattern.PLAY[times]
+
+    def loop (self):
+        self.play_counter = Pattern.PLAY["loop"]
+
+# ----- HARDWARE FUNCTIONS ----- #
+pattern_queues = []
+
+def register (pattern):
+    pattern_queues.append(pattern)
+
+def unregister (pattern):
+    del pattern_queues[pattern]
+
+def runpattern (pattern):
+    # TODO: Link this to hardware here
+    for row in pattern:
+        rowstr = ""
+
+        for col in row:
+            if col: rowstr += "X"
+            else: rowstr += " "
+
+        print(rowstr)
+
+def run ():
+    counter = 0;
+
+    while True:
+        if counter == len(pattern_queues): counter = 0
+        else:
+            # If the play-counter has a finite number of plays
+            if pattern_queues[counter].play_counter > 0:
+                runpattern(pattern_queues[counter].pattern)
+                pattern_queues[counter].play_counter -= 1
+
+            # If the play-counter is looping
+            elif pattern_queues[counter].play_counter < 0:
+                runpattern(pattern_queues[counter].pattern)
+
+        counter += 1
+
+
+# ----- SCRIPT ---- #
+run()
+
+# ----- TESTING CODE ----- #
+if __name__ == "__main__":
+    #
+    p = [[True for cols in range(COLS)] for rows in range(COLS)]
+    pattern = Pattern(p)
+    pattern.play(1)
+
+
